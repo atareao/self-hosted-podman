d32868e68bd33be9c86d9e44112b5f786f58b971
Lorenzo Carbonell a.k.a. atareao <lorenzo.carbonell.cerezo@gmail.com>
2026-02-15 13:10:24 +0000

    runtime

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,4 +1,6 @@
 if status is-interactive
+    set -gx XDG_RUNTIME_DIR /run/user/(id -u)
+    set -gx DBUS_SESSION_BUS_ADDRESS unix:path=$XDG_RUNTIME_DIR/bus
     if test -d ~/.local/bin
         fish_add_path ~/.local/bin
     end
