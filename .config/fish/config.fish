fd807c617603a4a279231f952d1cfa9f53d211a1
Lorenzo Carbonell a.k.a. atareao <lorenzo.carbonell.cerezo@gmail.com>
2026-02-15 08:40:49 +0000

    local-bin

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,5 +1,7 @@
 if status is-interactive
-    # Commands to run in interactive sessions can go here
+    if test -d ~/.local/bin
+        fish_add_path ~/.local/bin
+    end
     # zoxide
     zoxide init fish | source
     # starship
