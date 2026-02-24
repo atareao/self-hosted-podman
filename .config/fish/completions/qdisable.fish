7c698cae2d6b5f96707f569b2571823cbb54b0ec
Lorenzo Carbonell a.k.a. atareao <lorenzo.carbonell.cerezo@gmail.com>
2026-02-24 20:09:32 +0100

    ✨ Agrega archivos de completado para qenable y qdisable

---
+++ .config/fish/completions/qdisable.fish
@@ -0,0 +1,3 @@
+complete -c qdisable -f
+complete -c qdisable -a "(ls $HOME/.config/containers/systemd/)" -d "Stack de Quadlet activo"
+
---
+++ .config/fish/completions/qenable.fish
@@ -0,0 +1,2 @@
+complete -c qenable -f
+complete -c qenable -a "(ls $HOME/.config/containers/available/)" -d "Stack de Quadlet disponible"
